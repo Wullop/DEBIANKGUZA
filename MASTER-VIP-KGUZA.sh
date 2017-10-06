05aa74da3b2ef2a064ab3bb3b4b6619253ec2581
kguza <31211935+Wullop@users.noreply.github.com>
2017-10-06 15:48:42 +0700

    Add files via upload

---
+++ MASTER-VIP-KGUZA.sh
@@ -0,0 +1,72 @@
+
+echo <head>  <meta http-equiv="Content-Language" content="th"> 
+<meta http-equiv="content-Type" content="text/html; charset=window-874"> 
+<meta http-equiv="content-Type" content="text/html; charset=tis-620"> </head>
+<div id="intro">
+	<table border="2"  background="#A100FF">
+    <center>
+     <b>
+          <FONT COLOR="#FF0000" size="10">A</FONT>
+          <FONT COLOR="#FF9900" size="10">p</FONT>
+          <FONT COLOR="#FF0099" size="10">p</FONT>
+          <FONT COLOR="#99FF00" size="10">O</FONT>
+          <FONT COLOR="#00FF00" size="10">p</FONT>
+          <FONT COLOR="#00FF99" size="10">e</FONT>
+          <FONT COLOR="#00FFFF" size="10">n</FONT>
+          <FONT COLOR="#0099FF" size="10">V</FONT>
+          <FONT COLOR="#0000FF" size="10">P</FONT>
+          <FONT COLOR="#9900FF" size="10">N</FONT>
+          <FONT COLOR="#FFFFFF" size="10">-</FONT>
+          <FONT COLOR="#FF00FF" size="10">P</FONT>
+          <FONT COLOR="#FF0099" size="10">R</FONT>
+          <FONT COLOR="#FF0099" size="10">O</FONT>
+     </b>
+</center>
+<FONT COLOR="#FFFFFF" size="1">-</FONT>
+<center>
+                <tr>
+                        <td>
+                                <marquee behavior="alternate">
+                                        <img border="0" src="https://scrip.000webhostapp.com/Seller/20171005_130801_rmscr.jpg" height="1000" width="600"></a>
+                                        <img border="0" src="https://scrip.000webhostapp.com/Seller/20171005_130827_rmscr.jpg" height="1000" width="600"></a>
+                                        <img border="0" src="https://scrip.000webhostapp.com/Seller/20171005_130840_rmscr.jpg" height="1000" width="600"></a>
+                                        <img border="0" src="https://scrip.000webhostapp.com/Seller/20171005_130848_rmscr.jpg" height="1000" width="600"></a>
+                                        <img border="0" src="https://scrip.000webhostapp.com/Seller/20171005_130855_rmscr.jpg" height="1000" width="600"></a>
+                                </marquee>
+                        </td>
+                </tr>
+</center>
+</table>
+<FONT COLOR="#FFFFFF" size="1">-</FONT><FONT SIZE=30><B>
+<li>ดาวน์โหลดแอปSUPERVPN <a href><a href="https://www.mediafire.com/file/ksalmh3887qiltq/SUPERVPN+00.00.03.apk">»คลิก«</FONT></a href></li>
+<FONT SIZE=30><B>
+<li>สนใจติดต่อสอบถาม <a href><a href="http://line.me/ti/p/Dh6o2a5Ar9">» คลิก «</FONT></a href></li>
+<h1><font><font color="#ffffff">~</font></h1>
+<FONT SIZE=30><B><U>
+<marquee width="50%">บริการเช่า VPN</marquee></FONT></B></U>
+<lu>
+<h1><li>บริการเช่าVPN ระบบ VPS</li></h1>
+<h1><li>ใช้ได้ทั้ง ssh เเละ openvpn</li></h1>
+<h1><li>ราคาเพียง 20บาท/เดือน</li></h1>
+</ul>
+<h1><font><font color="#ffffff">~</font></h1>
+<FONT SIZE=30><B><U>
+<marquee width="50%">บริการรันScripVPS</marquee></FONT></B></U>
+<h1><li>debain 7 - 8</li></h1>
+<h1><li>ubuntu 14</li></h1>
+<h1><li>ค่าบริการ50฿/1เซิฟเวอร์</li></h1>
+<h1>
+<FONT COLOR="#FFFFFF" size="3">-</FONT>
+<marquee behavior="alternate">
+<FONT COLOR="#0099FF" size="10">ขอบ</FONT>
+<FONT COLOR="#0000FF" size="10">คุณ</FONT>
+<FONT COLOR="#9900FF" size="10">ที่</FONT>
+<FONT COLOR="#FF00FF" size="10">สนับ</FONT>
+<FONT COLOR="#FF0099" size="10">สนุน</FONT>
+<FONT COLOR="#0000FF" size="10">บริ</FONT>
+<FONT COLOR="#9900FF" size="10">การ</FONT>
+<FONT COLOR="#FF0099" size="10">ของ</FONT>
+<FONT COLOR="#0000FF" size="10">เรา</FONT>
+<FONT COLOR="#FF0099" size="10">!!!!@KGUZA!!!!</FONT>
+</marquee>
+ </div>
